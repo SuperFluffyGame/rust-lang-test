123b9e56bccb70b4826be0e3ac99d78fccc6a79c
SuperFluffyGame <81885757+SuperFluffyGame@users.noreply.github.com>
2022-07-05 16:21:58 -0500

    file

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,22 @@ mod lang;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    // let string = std::env::args().skip(1).next().unwrap();
+    let arg_path = std::env::args().skip(1).next().unwrap_or("".to_string());
 
-    let path = r"C:\Users\super\OneDrive\Documents\coding\RUST\math-parser\test.txt";
+    let base_path = std::env::current_dir()?;
+
+    let path;
+
+    if arg_path.starts_with("/") || arg_path.starts_with("./") {
+        path = base_path.join(arg_path);
+    } else {
+        path = std::path::PathBuf::from(arg_path);
+    }
+
+    if !path.exists() {
+        eprintln!("Path does not exist: '{}'", path.display());
+        std::process::exit(1);
+    }
 
     let mut file = std::fs::File::open(path)?;
     let mut contents = String::new();
@@ -17,9 +30,9 @@ fn main() -> std::io::Result<()> {
     let tokens = lang::tokenize(contents);
 
     if let Ok(tokens) = tokens {
-        println!("{:#?}", tokens);
+        println!("{:?}", tokens);
     } else if let Err(e) = tokens {
-        println!("{}", e);
+        eprintln!("{}", e);
     }
 
     Ok(())
